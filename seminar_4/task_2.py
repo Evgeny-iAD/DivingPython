d99fc200c6dc89a656c8dacb59f020baa61d85a4
iAD <iadmail1987@gmail.com>
2023-06-24 21:40:58 +0300

    task #2 finish

---
+++ seminar_4/task_2.py
@@ -0,0 +1,22 @@
+# Напишите функцию для транспонирования матрицы
+
+
+def transpose_matrix(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            transposed_matrix[j][i] = matrix[i][j]
+    return transposed_matrix
+
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    transposed = transpose_matrix(matrix)
+    for el in transposed:
+        print(el)
